531282b21c5368f6b6e81b31affc7a05482c009d
BXgus <138280188+BXgus@users.noreply.github.com>
2024-06-22 18:27:48 +0300

    #1

--- Progect__REGEX/Progect__REGEX.cpp
+++ Progect__REGEX/Progect__REGEX.cpp
@@ -12,9 +12,39 @@ int main()
         cout << "Regex found in Drizzle !!" << endl;
     // ---------------------------------------------------------
     const char* target = "2014-04-02";
+    // ---------------------------------------------------------
     regex rx2("[0-9]{4}(-[0-3][0-9]){2}");
     if (regex_match(target, target+strlen(target), rx2))
     {
         cout << "Regex found in 2014-04-02" << endl;
     }
+    // ============================================================================
+    std::regex_constants::match_flag_type  FONLY =
+                std::regex_constants::format_first_only;
+
+    std::regex  rx_1("a");
+    string  fmt_1("A");
+    string  str("adaeaf");
+    cout<< "replacement == " <<  regex_replace(str, rx_1, fmt_1) <<endl; // выведется replacement == AdAeAf
+    // ---------------------------------------------------------
+    cout<< "replacement == " << regex_replace(str, rx_1, fmt_1, FONLY) <<endl;
+    // -------------------------------------------------------------------
+    char  buf[20];
+    const char*  first = "axayaz";
+    const char*  last = first + strlen(first);
+    std::regex   rx_2("a");
+    std::string  fmt_2("A");
+    * ( std::regex_replace(&buf[0], first, last, rx_2, fmt_2) ) = '\0';
+    cout << "replacement == " << &buf[0] << endl; // выведется replacement == AxAyAz
+    // ---------------------------------------------------------
+    * ( std::regex_replace(&buf[0], first, last, rx_2, fmt_2, FONLY) ) = '\0';
+    cout << "replacement == " << &buf[0] << endl; // выведется replacement == Axayazё
+    // ---------------------------------------------------------
+    // ---------------------------------------------------------
+    // ---------------------------------------------------------
+    // ---------------------------------------------------------
+    // ---------------------------------------------------------
+    // ---------------------------------------------------------
+    // ---------------------------------------------------------
+
 }
